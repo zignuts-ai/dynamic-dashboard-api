0c80d43622b47b282bf3d6f59379e8367379fcaa
Vishnu Kerasiya <vishnuk@zignuts.com>
2024-12-07 20:27:21 +0530

    fix: module not found

--- api/models/index.js
+++ api/models/index.js
@@ -2,21 +2,10 @@ const { sequelize } = require('../../config/sequelize');
 
 const User = require('./User');
 const Admin = require('./Admin');
-const Dashboard = require('./Dashboard');
-const DataStore = require('./DataStore');
-const Organisation = require('./Organisation');
-const UserDataStoreOrganisationMap = require('./UserDataStoreOrganisationMap');
-const UserOragnisationMap = require('./UserOragnisationMap');
-const Wiedge = require('./Wiedge');
+
 
 module.exports = {
   User,
   Admin,
-  Dashboard,
-  DataStore,
-  Organisation,
-  UserDataStoreOrganisationMap,
-  UserOragnisationMap,
-  Wiedge,
   sequelize,
 };
