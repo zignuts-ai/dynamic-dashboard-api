6bb047b24923eb4509f96f79ac2e58b89d024cc1
Vishnu Kerasiya <vishnuk@zignuts.com>
2024-12-10 02:47:13 +0530

    feat: refector codes

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,6 @@ const express = require('express');
 // Create router
 const Router = express.Router();
 
-const AdminRoutes = require('./admin');
 const UserRoutes = require('./user');
 const ExampleROutes = require('./example');
 const ChatGptRoutes = require('./chatgpt');
@@ -15,7 +14,6 @@ const ChatRoutes = require('./chat');
 const AgentRoutes = require('./agent');
 
 // Use routers
-Router.use('/admin', AdminRoutes);
 Router.use('/user', UserRoutes);
 Router.use('/example', ExampleROutes);
 Router.use('/chatgpt', ChatGptRoutes);
